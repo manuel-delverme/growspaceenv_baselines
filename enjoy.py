e9305e9796b1b2b396d55c71c209f442bb8cb03f
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-09-01 08:23:31 -0400

    debug

--- enjoy.py
+++ enjoy.py
@@ -46,7 +46,8 @@ env = make_vec_envs(
     None,
     device='cpu',
     custom_gym=args.custom_gym,
-    allow_early_resets=False)
+    navi=False,
+    allow_early_resets=False, enjoy=True)
 
 # Get a render function
 render_func = get_render_func(env)
