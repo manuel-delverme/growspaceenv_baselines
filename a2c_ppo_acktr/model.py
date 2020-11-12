350f4b4135ff0300abc10c28efee29bd1ead6fc4
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-11-12 17:53:06 -0500

    change in line 201, not divide by 255 since binary

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -198,7 +198,7 @@ class CNNBase(NNBase):
         self.train()
 
     def forward(self, inputs, rnn_hxs, masks):
-        x = self.main(inputs / 255.0)
+        x = self.main(inputs)# / 255.0)
         #print("what is X:", x)
 
         if self.is_recurrent:
