12cf1d804dd01217cbeb49aff5c4ed6776afec4b
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2021-02-10 13:04:26 -0500

    limit total number of env steps to 1million

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -123,7 +123,7 @@ def get_args():
     parser.add_argument(
         '--num-env-steps',
         type=int,
-        default=10e6,
+        default=1e6,
         help='number of environment steps to train (default: 10e6)')
     parser.add_argument(
         '--env-name',
