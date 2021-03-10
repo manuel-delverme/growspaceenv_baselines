36f8003086667239d61f4de62e014c7308fffd7f
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-10 13:37:38 +0100

    added sgd optimizer

--- a2c_ppo_acktr/algo/ppo.py
+++ a2c_ppo_acktr/algo/ppo.py
@@ -5,17 +5,20 @@ import torch.optim as optim
 
 
 class PPO():
-    def __init__(self,
-                 actor_critic,
-                 clip_param,
-                 ppo_epoch,
-                 num_mini_batch,
-                 value_loss_coef,
-                 entropy_coef,
-                 lr=None,
-                 eps=None,
-                 max_grad_norm=None,
-                 use_clipped_value_loss=True):
+    def __init__(
+            self,
+            actor_critic,
+            clip_param,
+            ppo_epoch,
+            num_mini_batch,
+            value_loss_coef,
+            entropy_coef,
+            lr=None,
+            eps=None,
+            max_grad_norm=None,
+            use_clipped_value_loss=True,
+            optimizer="adam",
+    ):
 
         self.actor_critic = actor_critic
 
@@ -29,12 +32,15 @@ class PPO():
         self.max_grad_norm = max_grad_norm
         self.use_clipped_value_loss = use_clipped_value_loss
 
-        self.optimizer = optim.Adam(actor_critic.parameters(), lr=lr, eps=eps)
+        if optimizer == "adam":
+            self.optimizer = optim.Adam(actor_critic.parameters(), lr=lr, eps=eps)
+        elif optimizer == "sgd":
+            self.optimizer = optim.SGD(actor_critic.parameters(), lr=lr, momentum=0.90)
 
     def update(self, rollouts):
         advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
         advantages = (advantages - advantages.mean()) / (
-            advantages.std() + 1e-5)
+                advantages.std() + 1e-5)
 
         value_loss_epoch = 0
         action_loss_epoch = 0
@@ -50,13 +56,14 @@ class PPO():
 
             for sample in data_generator:
                 obs_batch, recurrent_hidden_states_batch, actions_batch, \
-                   value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
-                        adv_targ = sample
+                value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
+                adv_targ = sample
 
                 # Reshape to do in a single forward pass for all steps
                 values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
                     obs_batch, recurrent_hidden_states_batch, masks_batch,
-                    actions_batch)
+                    actions_batch
+                )
 
                 ratio = torch.exp(action_log_probs -
                                   old_action_log_probs_batch)
@@ -67,20 +74,20 @@ class PPO():
 
                 if self.use_clipped_value_loss:
                     value_pred_clipped = value_preds_batch + \
-                        (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
+                                         (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
                     value_losses = (values - return_batch).pow(2)
                     value_losses_clipped = (
-                        value_pred_clipped - return_batch).pow(2)
-                    value_loss = 0.5 * torch.max(value_losses,
-                                                 value_losses_clipped).mean()
+                            value_pred_clipped - return_batch).pow(2)
+                    value_loss = 0.5 * torch.max(value_losses, value_losses_clipped).mean()
                 else:
                     value_loss = 0.5 * (return_batch - values).pow(2).mean()
 
                 self.optimizer.zero_grad()
-                (value_loss * self.value_loss_coef + action_loss -
-                 dist_entropy * self.entropy_coef).backward()
-                nn.utils.clip_grad_norm_(self.actor_critic.parameters(),
-                                         self.max_grad_norm)
+
+                (value_loss * self.value_loss_coef + action_loss - dist_entropy * self.entropy_coef).backward()
+
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+
                 self.optimizer.step()
 
                 value_loss_epoch += value_loss.item()
