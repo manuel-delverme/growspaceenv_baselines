251639db10ca49abeef100aa95c6c46a36827a94
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-24 18:22:55 +0100

    remove duplicate line

--- main.py
+++ main.py
@@ -58,9 +58,6 @@ def main():
     torch.set_num_threads(1)
     device = torch.device("cuda:0" if args.cuda else "cpu")
 
-    envs = make_vec_envs(
-        args.env_name, args.seed, args.num_processes, args.gamma, args.log_dir, device, False, args.custom_gym
-    )
     envs = make_vec_envs(args.env_name, args.seed, args.num_processes,
                          args.gamma, args.log_dir, device, False, args.custom_gym)
 
