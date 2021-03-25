def663f5b349b6db801ef974c96cb74fe346072d
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-26 00:39:58 +0100

    changes to add buddy

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import experiment_buddy
+import torch
 
 FIRST_BRANCH_HEIGHT = .24
 BRANCH_THICCNESS = 0.015
@@ -6,7 +7,40 @@ BRANCH_LENGTH = 1 / 9
 MAX_BRANCHING = 10
 LIGHT_WIDTH = .25
 LIGHT_DIF = 250
+algo = "ppo"
+gail = False
+gail_experts_dir = './gail_experts'
+gail_batch_size = 128
+gail_epoch = 5
+lr = 2.5e-4
+eps = 1e-5
+alpha = 0.99
+gamma = 0.99
+use_gae = True
+gae_lambda = 0.95
+entropy_coef = 0.01
+value_loss_coef = 0.5
+max_grad_norm = 0.5
+seed = 1
+cuda_deterministic = False
+num_processes = 1
+num_steps = 2500
+custom_gym = "growspace"
+ppo_epoch = 4
+num_mini_batch = 32
+clip_param = 0.1
+log_interval = 10
+save_interval = 100
+eval_interval = None
+num_env_steps = 1e6
+env_name = "GrowSpaceSpotlight-Mnist5-v0"
+log_dir = "/tmp/gym/"
+save_dir = "./trained_models/"
+use_proper_time_limits = False
+recurrent_policy = False
+use_linear_lr_decay = True
+no_cuda = False
+cuda = not no_cuda and torch.cuda.is_available()
 
 experiment_buddy.register(locals())
-
 tensorboard = experiment_buddy.deploy("mila", sweep_yaml="", entity='growspace')
--- main.py
+++ main.py
@@ -17,7 +17,6 @@ import torch.optim as optim
 import wandb
 from a2c_ppo_acktr import algo, utils
 from a2c_ppo_acktr.algo import gail
-from a2c_ppo_acktr.arguments import get_args
 from a2c_ppo_acktr.envs import make_vec_envs
 from a2c_ppo_acktr.model import Policy
 from a2c_ppo_acktr.storage import RolloutStorage
@@ -26,42 +25,43 @@ import os
 os.environ['OPENCV_IO_MAX_IMAGE_PIXELS']=str(2**84)
 import cv2
 from comet_ml import Experiment
+import config
 
 def main():
-    args = get_args()
 
     #if comet_loaded:
        # experiment = Experiment(
             #api_key="WRmA8ms9A78K85fLxcv8Nsld9",
             #project_name="growspace-tests",
             #workspace="yasmeenvh")
-        #experiment.set_name(args.comet)
-        #for key, value in vars(args).items():
+        #experiment.set_name(config.comet)
+        #for key, value in vars(config).items():
             #experiment.log_parameter(key, value)
     #else:
         #experiment = None
-    wandb.init(settings=wandb.Settings(start_method="fork"),project='ppo', entity='growspace')
+    # wandb.init(settings=wandb.Settings(start_method="fork"),project='ppo', entity='growspace')
     #wandb.config()
+    wandb.run = config.tensorboard.run
 
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
 
-    if args.cuda and torch.cuda.is_available() and args.cuda_deterministic:
+    if config.cuda and torch.cuda.is_available() and config.cuda_deterministic:
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-    log_dir = os.path.expanduser(args.log_dir)
+    log_dir = os.path.expanduser(config.log_dir)
     eval_log_dir = log_dir + "_eval"
     utils.cleanup_log_dir(log_dir)
     utils.cleanup_log_dir(eval_log_dir)
 
     torch.set_num_threads(1)
-    device = torch.device("cuda:0" if args.cuda else "cpu")
+    device = torch.device("cuda:0" if config.cuda else "cpu")
 
-    envs = make_vec_envs(args.env_name, args.seed, args.num_processes,
-                         args.gamma, args.log_dir, device, False, args.custom_gym)
+    envs = make_vec_envs(config.env_name, config.seed, config.num_processes,
+                         config.gamma, config.log_dir, device, False, config.custom_gym)
 
-    if "Mnist" in args.env_name:
+    if "Mnist" in config.env_name:
         base = 'Mnist'
     else:
         base = None
@@ -70,52 +70,52 @@ def main():
         envs.observation_space.shape,
         envs.action_space,
         base,
-        base_kwargs={'recurrent': args.recurrent_policy})
+        base_kwargs={'recurrent': config.recurrent_policy})
     actor_critic.to(device)
 
-    if args.algo == 'a2c':
+    if config.algo == 'a2c':
         agent = algo.A2C_ACKTR(
             actor_critic,
-            args.value_loss_coef,
-            args.entropy_coef,
-            lr=args.lr,
-            eps=args.eps,
-            alpha=args.alpha,
-            max_grad_norm=args.max_grad_norm)
-    elif args.algo == 'ppo':
+            config.value_loss_coef,
+            config.entropy_coef,
+            lr=config.lr,
+            eps=config.eps,
+            alpha=config.alpha,
+            max_grad_norm=config.max_grad_norm)
+    elif config.algo == 'ppo':
         agent = algo.PPO(
             actor_critic,
-            args.clip_param,
-            args.ppo_epoch,
-            args.num_mini_batch,
-            args.value_loss_coef,
-            args.entropy_coef,
-            lr=args.lr,
-            eps=args.eps,
-            max_grad_norm=args.max_grad_norm)
-    elif args.algo == 'acktr':
+            config.clip_param,
+            config.ppo_epoch,
+            config.num_mini_batch,
+            config.value_loss_coef,
+            config.entropy_coef,
+            lr=config.lr,
+            eps=config.eps,
+            max_grad_norm=config.max_grad_norm)
+    elif config.algo == 'acktr':
         agent = algo.A2C_ACKTR(
-            actor_critic, args.value_loss_coef, args.entropy_coef, acktr=True)
+            actor_critic, config.value_loss_coef, config.entropy_coef, acktr=True)
 
-    if args.gail:
+    if config.gail:
         assert len(envs.observation_space.shape) == 1
         discr = gail.Discriminator(
             envs.observation_space.shape[0] + envs.action_space.shape[0], 100,
             device)
         file_name = os.path.join(
-            args.gail_experts_dir, "trajs_{}.pt".format(
-                args.env_name.split('-')[0].lower()))
+            config.gail_experts_dir, "trajs_{}.pt".format(
+                config.env_name.split('-')[0].lower()))
         
         expert_dataset = gail.ExpertDataset(
             file_name, num_trajectories=4, subsample_frequency=20)
-        drop_last = len(expert_dataset) > args.gail_batch_size
+        drop_last = len(expert_dataset) > config.gail_batch_size
         gail_train_loader = torch.utils.data.DataLoader(
             dataset=expert_dataset,
-            batch_size=args.gail_batch_size,
+            batch_size=config.gail_batch_size,
             shuffle=True,
             drop_last=drop_last)
 
-    rollouts = RolloutStorage(args.num_steps, args.num_processes,
+    rollouts = RolloutStorage(config.num_steps, config.num_processes,
                               envs.observation_space.shape, envs.action_space,
                               actor_critic.recurrent_hidden_state_size)
 
@@ -138,20 +138,20 @@ def main():
 
     start = time.time()
     num_updates = int(
-        args.num_env_steps) // args.num_steps // args.num_processes
+        config.num_env_steps) // config.num_steps // config.num_processes
     #print("what are the num_updates",num_updates)
     x = 0
     #s = 0
     #av_ep_move = []
     for j in range(num_updates):
 
-        if args.use_linear_lr_decay:
+        if config.use_linear_lr_decay:
             # decrease learning rate linearly
             utils.update_linear_schedule(
                 agent.optimizer, j, num_updates,
-                agent.optimizer.lr if args.algo == "acktr" else args.lr)
+                agent.optimizer.lr if config.algo == "acktr" else config.lr)
         #new_branches = []
-        for step in range(args.num_steps):
+        for step in range(config.num_steps):
             #if s == 0:
                 #light_move_ep = []
             #s +=1
@@ -226,24 +226,24 @@ def main():
         #episode_branches.append(np.asarray([[np.mean(new_branches)]]))
         #print("after")
         #print(episode_branches)
-        if args.gail:
+        if config.gail:
             if j >= 10:
                 envs.venv.eval()
 
-            gail_epoch = args.gail_epoch
+            gail_epoch = config.gail_epoch
             if j < 10:
                 gail_epoch = 100  # Warm up
             for _ in range(gail_epoch):
                 discr.update(gail_train_loader, rollouts,
                              utils.get_vec_normalize(envs)._obfilt)
 
-            for step in range(args.num_steps):
+            for step in range(config.num_steps):
                 rollouts.rewards[step] = discr.predict_reward(
-                    rollouts.obs[step], rollouts.actions[step], args.gamma,
+                    rollouts.obs[step], rollouts.actions[step], config.gamma,
                     rollouts.masks[step])
 
-        rollouts.compute_returns(next_value, args.use_gae, args.gamma,
-                                 args.gae_lambda, args.use_proper_time_limits)
+        rollouts.compute_returns(next_value, config.use_gae, config.gamma,
+                                 config.gae_lambda, config.use_proper_time_limits)
 
         value_loss, action_loss, dist_entropy = agent.update(rollouts)
 
@@ -251,9 +251,9 @@ def main():
 
 
         # save for every interval-th episode or for the last epoch
-        if (j % args.save_interval == 0
-                or j == num_updates - 1) and args.save_dir != "":
-            save_path = os.path.join(args.save_dir, args.algo)
+        if (j % config.save_interval == 0
+                or j == num_updates - 1) and config.save_dir != "":
+            save_path = os.path.join(config.save_dir, config.algo)
             try:
                 os.makedirs(save_path)
             except OSError:
@@ -262,10 +262,10 @@ def main():
             torch.save([
                 actor_critic,
                 getattr(utils.get_vec_normalize(envs), 'ob_rms', None)
-            ], os.path.join(save_path, args.env_name + ".pt"))
+            ], os.path.join(save_path, config.env_name + ".pt"))
 
-        if j % args.log_interval == 0 and len(episode_rewards) > 1:
-            total_num_steps = (j + 1) * args.num_processes * args.num_steps
+        if j % config.log_interval == 0 and len(episode_rewards) > 1:
+            total_num_steps = (j + 1) * config.num_processes * config.num_steps
             end = time.time()
             wandb.log({"Reward Min": np.min(episode_rewards)}, step=total_num_steps)
             wandb.log({"Episode Reward": episode_rewards}, step=total_num_steps)
@@ -346,11 +346,11 @@ def main():
             episode_success.clear()
 
 
-        if (args.eval_interval is not None and len(episode_rewards) > 1
-                and j % args.eval_interval == 0):
+        if (config.eval_interval is not None and len(episode_rewards) > 1
+                and j % config.eval_interval == 0):
             ob_rms = utils.get_vec_normalize(envs).ob_rms
-            evaluate(actor_critic, ob_rms, args.env_name, args.seed,
-                     args.num_processes, eval_log_dir, device)
+            evaluate(actor_critic, ob_rms, config.env_name, config.seed,
+                     config.num_processes, eval_log_dir, device)
 
 
 if __name__ == "__main__":
