b3545964624078131b547fcafc4d1e07d659415f
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-07 15:20:57 +0200

    add optimizer to config, TODO sweep with optimizer

--- config.py
+++ config.py
@@ -42,11 +42,12 @@ recurrent_policy = False
 use_linear_lr_decay = True
 no_cuda = False
 cuda = not no_cuda and torch.cuda.is_available()
+optimizer = "adam"
 
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
-    "mila",
-    sweep_yaml="sweep.yaml",
-    proc_num=10,
+    "",
+    sweep_yaml="",
+    proc_num=1,
     wandb_kwargs={"entity": "growspace"}
 )
